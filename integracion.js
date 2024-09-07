68ab5f6dfc98def412342419c5c8f57bf2be7a6c
antxnyiuuuu <antonychaguamate@gmail.com>
2024-09-07 16:19:27 -0500

    Pagina creada para que agreguen sus partes en el html

---
+++ integracion.js
@@ -0,0 +1,70 @@
+let materias =[
+    
+]
+mostrarPaginaMaterias=function(){
+
+    mostrarComponente("divMaterias");
+    ocultarComponente("divGestionEstudiante");
+    ocultarComponente("divGestionAulas");
+    ocultarComponente("divGestionProfesores")
+
+}
+
+crearMateria=function(
+){
+    let materia={}
+    let nombreMateria=recuperarTexto("txtNombreMaterias")
+    let codigo=recuperarTexto("txtCodigo")
+    let descripcion=recuperarTexto("txtDescripcion")
+    materia.nombreMateria=nombreMateria
+    materia.codigo=codigo
+    materia.descripcion=descripcion
+    return materia
+}
+
+agregarMateria=function(){
+    materiaCreada=crearMateria()
+    materias.push(materiaCreada)
+    console.log(materiaCreada)
+}
+
+
+
+refrescarMaterias=function(){
+    let cmpTabla=document.getElementById("tblMaterias")
+    let contenidoTabla="<table><tr>"+
+    "<th>NOMBRE</th>"+
+    "<th>CODIGO</th>"+
+    "<th>DESCRIPCION</th>"+
+    "</tr>";
+    let resultado
+    for (let i=0;i<materias.length;i++){
+        resultado=materias[i];
+        contenidoTabla+=
+        "<tr><td>"+resultado.nombreMateria
+        +"</td><td>"+resultado.codigo
+        +"</td><td>"+resultado.descripcion+"</td></tr>"
+    } 
+    contenidoTabla+="</table>"
+    cmpTabla.innerHTML=contenidoTabla;
+
+    
+}
+
+
+buscarMateria=function(identificador){
+    identificador=recuperarTexto("txtBuscar")    
+    for (let i=0;i<materias.length;i++){
+        resultado=materias[i];
+        let codigo=resultado.codigo
+        if(identificador==codigo){              
+            alert("MATERIA ENCONTRADA")
+            break}
+
+        else{
+            alert("MATERIA NO ENCONTRADA")
+        }
+    }
+    
+
+}
