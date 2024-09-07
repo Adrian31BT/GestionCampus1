3bdb7333552e7631f4b94ff817b84c24bd4c38f8
kevinariel18 <kevincata2005@gmail.com>
2024-09-07 16:35:26 -0500

    aulas

--- utilitarios.js
+++ utilitarios.js
@@ -35,46 +35,4 @@ recuperarFloat = function(idComponente){
     return valorFlotante;
  }
 
-mostrarComponente = function(idComponente){
-    document.getElementById(idComponente).style.display = "block";
-}
 
-ocultarComponente = function(idComponente){
-    document.getElementById(idComponente).style.display = "none";
-}
-
-deshabilitarComponente = function(idComponente){
-    document.getElementById(idComponente).disabled = true;
-}
-
-habilitarComponente = function(idComponente){
-    document.getElementById(idComponente).disabled = false;
-}
-
-esMayuscula=function(caracter){
-    let cadena;
-    let mayuscula=false
-    cadena=caracter.charCodeAt(0);
-    if(cadena>=65 && cadena<=90){
-        mayuscula=true
-    }
-    return mayuscula;
-}
-recuperarTextoDiv=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.textContent;
-    return valorIngresado;
-}
-recuperarIntDiv = function(idComponente){
-    let valorCaja= recuperarTextoDiv(idComponente);
-    let valorEntero = parseInt(valorCaja);
-    return valorEntero;
- }
- 
- recuperarFloatDiv = function(idComponente){
-     let valorCaja= recuperarTextoDiv(idComponente);
-     let valorFlotante = parseFloat(valorCaja);
-     return valorFlotante;
-  }
